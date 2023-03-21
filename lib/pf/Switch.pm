4a91818364c1812b9fc1fa7e456e355468b21933
James Rouzier <jrouzier@inverse.ca>
2023-03-21 15:06:33 +0000

    Don't use SNMP if it is disabled

--- lib/pf/Switch.pm
+++ lib/pf/Switch.pm
@@ -214,6 +214,10 @@ sub isUpLink {
 sub connectRead {
     my $self   = shift;
     my $logger = $self->logger;
+    if (isdisabled($self->{_useSNMP)) {
+        return 0;
+    }
+
     if ( defined( $self->{_sessionRead} ) ) {
         return 1;
     }
@@ -335,12 +339,12 @@ sub cachedSNMPRequest {
 sub disconnectRead {
     my $self   = shift;
     my $logger = $self->logger;
-    if ( !defined( $self->{_sessionRead} ) ) {
-        return 1;
+    if ( defined( $self->{_sessionRead} ) ) {
+        $logger->debug( "closing SNMP v" . $self->{_SNMPVersion} . " read connection to $self->{_id}" );
+        $self->{_sessionRead}->close;
+        $self->{_sessionRead} = undef;
     }
-    $logger->debug( "closing SNMP v" . $self->{_SNMPVersion} . " read connection to $self->{_id}" );
-    $self->{_sessionRead}->close;
-    $self->{_sessionRead} = undef;
+
     return 1;
 }
 
@@ -354,6 +358,9 @@ It performs a write test to make sure that the write actually works.
 sub connectWriteTo {
     my ($self, $ip, $sessionKey,$port) = @_;
     my $logger = $self->logger;
+    if (isdisabled($self->{_useSNMP})) {
+        return 0;
+    }
 
     # if connection already exists, no need to connect again
     return 1 if ( defined( $self->{$sessionKey} ) );
