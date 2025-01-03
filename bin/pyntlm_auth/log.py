4ff8d7fc3ffdcbb54b7b06e7b2e67447c9470998
Zhihao Ma <zma@akamai.com>
2025-01-03 13:02:48 -0500

    adds default log level support

--- bin/pyntlm_auth/log.py
+++ bin/pyntlm_auth/log.py
@@ -1,8 +1,27 @@
 import logging
+import os
 
 
 def init_logging():
+    available_logging_levels = {'DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'}
+    logging_level = os.getenv('LOG_LEVEL')
+
+    if not logging_level:
+        logging_level = 'DEBUG'
+
+    if logging_level not in available_logging_levels:
+        logging_level = 'DEBUG'
+
     default_logging_level = logging.DEBUG
+
+    if logging_level == 'INFO':
+        default_logging_level = logging.INFO
+    elif logging_level == 'WARNING':
+        default_logging_level = logging.WARNING
+    elif logging_level == 'ERROR':
+        default_logging_level = logging.ERROR
+    elif logging_level == 'CRITICAL':
+        default_logging_level = logging.CRITICAL
 
     logger = logging.getLogger("ntlm-auth")
     logger.setLevel(default_logging_level)
